ed61047603791f36d4228b6ca1798c26a9933e15
Rubén Manzano Santacruz <rmanzanointecrobots@gmail.com>
2024-10-31 18:28:35 +0100

    commit

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 
@@ -7,29 +10,28 @@ export default function Intro() {
       <div className="flex items-center justify-center">
         <div className="flex space-x-4">
           <div className="relative h-24 w-24">
-            <Image
-              src="/ropon.jpeg"
-              alt="Rubén Profile Pic"
-              layout="fill"
-              quality="80"
-              priority={true}
-              className="rounded-full shadow-2xl"
-            />
-            <div className="absolute inset-0 rounded-full shadow-2xl"></div>
-          </div>
-          <div className="relative h-24 w-24">
-            <Image
-              src="/favicon.jpeg"
-              alt="icon"
-              layout="fill"
-              quality="80"
-              priority={true}
-              className="rounded-full shadow-2xl"
-            />
-            <div className="absolute inset-0 rounded-full shadow-2xl"></div>
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "tween",
+                duration: 0.2,
+              }}
+            >
+              <Image
+                src="/ropon.jpeg"
+                alt="Rubén Profile Pic"
+                layout="fill"
+                quality="80"
+                priority={true}
+                className="rounded-full shadow-2xl"
+              />
+            </motion.div>
+            <motion.span className="absolute bottom-0 right-0 text-4xl">
+              👋
+            </motion.span>
           </div>
         </div>
-        <span className="text-xl">👋 Hola muy buenas</span>
       </div>
     </section>
   );
